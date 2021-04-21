6bc4432ba6ba28252fc0e311c56097808f44576f
h576347 <42640829+h576347@users.noreply.github.com>
2021-04-21 11:14:59 +0200

    Update TryMes-brukertabeller.sql
    Endret slik at det blei riktig i oppdatering av databasen

--- SQL/TryMes-brukertabeller.sql
+++ SQL/TryMes-brukertabeller.sql
@@ -7,14 +7,10 @@ set search_path = bruker_tabeller;
 
 create table bruker(
 	bruker_id serial,
-	aktivitet_id int,
 	alder_id int,
-	resultat_id int, 
 	omraade_id int,
 	constraint bruker_PK primary key(bruker_id)
-	--constraint aktivitet_FK foreign key (aktivitet_id) references aktivitet(aktivitet_id),
 	--constraint aldersgruppe_FK foreign key (alder_id) references aldersgruppe(alder_id),
-	--constraint resultat_FK foreign key (resultat_id) references resultat(resultat_id),
 	--constraint omraade_FK foreign key (omraade_id) references omraade(omraade_id)
 );
 
@@ -81,20 +77,19 @@ create table kategori(
 	constraint kategori_PK primary key(kategori_id)
 );
 
-create table resultat (
-	resultat_id serial,
+create table bruker_aktivitet (
+	bruker_aktivitet_id serial,
+	bruker_id int,
 	aktivitet_id int,
 	skaare int,
 	treff int,
 	aktuell int,
 	styrke int,
-	constraint resultat_PK primary key(resultat_id)
+	constraint bruker_aktivitet_PK primary key(bruker_aktivitet_id)
 );
 
 alter table bruker
-add constraint aktivitet_FK foreign key (aktivitet_id) references aktivitet(aktivitet_id),
 add constraint aldersgruppe_FK foreign key (alder_id) references aldersgruppe(alder_id),
-add constraint resultat_FK foreign key (resultat_id) references resultat(resultat_id),
 add constraint omraade_FK foreign key (omraade_id) references omraade(omraade_id);
 
 alter table egenskap
@@ -112,4 +107,7 @@ add constraint egenskap_FK foreign key(egenskap_id) references egenskap(egenskap
 add constraint aldersgruppe_FK foreign key(alder_id) references aldersgruppe(alder_id),
 add constraint forbund_FK foreign key(forbund_id) references forbund(forbund_id);
 
+alter table bruker_aktivitet
+add constraint bruker_FK foreign key (bruker_id) references bruker(bruker_id),
+add constraint aktivitet_FK foreign key (aktivitet_id) references aktivitet(aktivitet_id);
 
