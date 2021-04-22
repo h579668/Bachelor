eddddd0f2f8cc59c1a7e88baa2a6b7f436693872
h576347 <42640829+h576347@users.noreply.github.com>
2021-04-22 19:14:07 +0200

    Update app.js

--- vs_code/app.js
+++ vs_code/app.js
@@ -1,3 +1,8 @@
+/*
+* This code connects node.js to the database(postgreSQL), stored in dBeaver.
+*/
+
+// Creating the client, and prepare to connect
 const { Client } = require('pg');
 const client = new Client({
 user: 'TryMes',
@@ -7,10 +12,14 @@ password: 'pass',
 port: 5434,
 })
 
+//Connecting to the database
 client.connect()
 
+//setting the path to bruker_tabeller
 client.query("SET search_path TO 'bruker_tabeller';")
 
+//Just a check to see if all the categories "kategorier" can be fiund,
+// and printed in the console.
 client.query('SELECT* from kategori', (err, res) => {
 console.log(err, res)
 client.end()
