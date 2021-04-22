7e5918edc50a1cf60d1f96eb17ed267f7237ec4e
h576347 <42640829+h576347@users.noreply.github.com>
2021-04-22 19:09:12 +0200

    Update app.js
    La inn de endringene som skal til for at databasen og js snakker sammen

--- vs_code/app.js
+++ vs_code/app.js
@@ -1,1 +1,17 @@
-const name = "hello";
+const { Client } = require('pg');
+const client = new Client({
+user: 'TryMes',
+host: 'data1.hib.no',
+database: 'TryMes',
+password: 'pass',
+port: 5434,
+})
+
+client.connect()
+
+client.query("SET search_path TO 'bruker_tabeller';")
+
+client.query('SELECT* from kategori', (err, res) => {
+console.log(err, res)
+client.end()
+})
