1fb523aa1b22958cecee746afc19ac3296a467a4
h579690 <h579690@stud.hvl.no>
2021-04-29 14:10:58 +0200

    Update HomePage.vue

--- trymes-nettside/src/components/HomePage.vue
+++ trymes-nettside/src/components/HomePage.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="home">
     <h1>{{ msg }}</h1>
-    <div id="quizBtn">
-        <router-link to="/quiz">Start test</router-link>
-    </div>
+    <router-link :to="{name: 'Quiz'}">
+      <button id="quizButton" class="foo bar">Start test</button>
+    </router-link>
     <p>Og finn 10 passende aktiviteter nær deg! </p>
     
-    <div id="divWhy">
+    <div id="info1">
         <h3>Hvorfor ta quizen</h3>
         <div class="text">Her kommer tekst</div>
     </div>
 
-    <div id="divDidUKnow">
+    <div id="info2">
         <h3>Visste du at?</h3>
         <div class="text"> Her kommer tekst</div>
     </div>
 
-    <div id="divInfo">
+    <div id="info3">
         <h3>Info</h3>
         <div class="text">Her kommer tekst</div>
     </div>
 
-    
   </div>
 </template>
 
@@ -37,26 +36,32 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+#quizButton {
+  background-color: #424242;
+  color:#ffffff;
+  font-size: 30px;
+}
+
 h3 {
   margin: 40px 0 0;
   font-family: 'Courier New', Courier, monospace;
 }
 
-#divWhy {
+#info1 {
     position: absolute;
     left: 30px;
     width: 30%;
     height: auto;
 }
 
-#divDidUKnow {
+#info2 {
     position: absolute;
     width: 30%;
     right: 35%;
     left: 35%;
     height: auto;
 }
-#divInfo {
+#info3 {
     position: absolute;
     right: 30px;
     width: 30%;
@@ -64,10 +69,12 @@ h3 {
 }
 
 .text {
-    background-color: grey;
+    background-color: lightgray;
     height: 200px;
     padding: 15px;
     margin: 5px;
 }
+
+
 
 </style>
