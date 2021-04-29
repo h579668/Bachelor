1d71b2384472dde63c6ff7d554587d351282a23d
h576347 <42640829+h576347@users.noreply.github.com>
2021-04-29 15:44:13 +0200

    Fikset questionPage + Quiz
    ENDELIG

---
+++ trymes-nettside/src/components/QuestionPage.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div>
+        <h1> {{ title}} </h1>
+        {{ information }} 
+      <div v-for="item in quizItems" :key="item.question">
+          <h2> {{ item.question }}</h2>
+          <input type="radio" :name= "item.id " value="God" />
+          <label> God </label><br />
+           <input type="radio" :name= "item.id" value="Noe god" />
+          <label> Noe god </label><br />
+          <input  type="radio" :name= "item.id" value="Ikke god" />
+          <label> Ikke god </label>
+        </div>
+    </div>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.11" type="JavaScript"></script>
+<script>
+export default {
+   name: "QuestionPage",
+   props: ['title', 'information'], //INFORMATION DOES NOT WORK 
+        data() {
+        return {
+              quizItems: [
+                { id: '1', question: 'Hvor god er du i styrke?' },
+                { id: '2', question: 'Hvor god er du i balanse?' },
+                { id: '3', question: 'Hvor god hurtighet har du?(Hvor raskt kan du løpe?)' },
+                { id: '4', question: 'Hvor god utholdenhet har du?(Kan du løpe/gå lenge?' },
+                { id: '5', question: 'Er du god i orientering?(Lese kart og løpe til riktige avmerkede punkter)' } 
+             ]};
+        },
+        
+    };
+</script>
+
+<style scoped>
+
+</style>
+
--- trymes-nettside/src/views/Quiz.vue
+++ trymes-nettside/src/views/Quiz.vue
@@ -1,61 +1,21 @@
 <template>
-  <div class="quiz">
-    <h1>Velkommen til test-siden!</h1>
-    <p>Informasjonstekst: Hei og hå, stå no på!</p>
-    <div>
-      <h2> Hvor god er du i styrke?</h2>
-      <input type="radio" name="answer1" value="God" />
-      <label> God </label><br />
-      <input type="radio" name="answer1" value="Noe god" />
-      <label> Noe god </label><br />
-      <input type="radio" name="answer1" value="Ikke god" />
-      <label> Ikke god </label>
-
-      <h2>Hvor god er du i hurtighet?</h2>
-      <input type="radio" name="answer2" value="God" />
-      <label> God </label><br />
-      <input type="radio" name="answer2" value="Noe god" />
-      <label> Noe god </label><br />
-      <input type="radio" name="answer2" value="Ikke god" />
-      <label> Ikke god </label>
-
-      <h2>Hvor god utholdenhet har du</h2>
-      <input type="radio" name="answer3" value="God" />
-      <label> God </label><br />
-      <input type="radio" name="answer3" value="Noe god" />
-      <label> Noe god </label><br />
-      <input type="radio" name="answer3" value="Ikke god" />
-      <label> Ikke god </label>
-
-      <h2>Hvor god balanse har du?</h2>
-      <input type="radio" name="answer4" value="God" />
-      <label> God </label><br />
-      <input type="radio" name="answer4" value="Noe god" />
-      <label> Noe god </label><br />
-      <input type="radio" name="answer4" value="Ikke god" />
-      <label> Ikke god </label>
-    </div>
+  <div >
+    <QuestionPage title = "Velkommen til test-siden!" />
+    <QuestionPage information = "Hei og hå, stå no på!" />
   </div>
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>
-<script>/*
-new Vue({
-  el: "#quiz",
-  data() {
-    return {
-      quizItems: {
-        question1:"Hvor god er du i styrke?",
-        question2:"Hvor god er du i balanse?",
-        question3:"Hvor god hurtighet har du?(Hvor raskt kan du løpe?)",
-        question4:"Hvor god utholdenhet har du?(Kan du løpe/gå lenge?",
-      }
-    };
-  }
-});*/
-  export default {
-    
-  }
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.11" type="JavaScript"></script>
+
+<script>
+import QuestionPage from "@/components/QuestionPage.vue";
+
+export default {
+  name: "Quiz",
+  components: {
+     QuestionPage,
+  },
+};
 </script>
 
 <style scoped>
