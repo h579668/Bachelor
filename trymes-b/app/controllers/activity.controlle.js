4a6cbb771b7efa244869ccab9da10d37170c7341
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-04 11:53:06 +0200

    Opprettet activity.contoller.js

---
+++ trymes-b/app/controllers/activity.controlle.js
@@ -0,0 +1,146 @@
+const db = require("../models");
+const Activity = db.activites;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Actvity
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create an Actvity
+  const actvity = {
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false
+  };
+
+  // Save Actvity in the database
+  Activity.create(activity)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Actvity."
+      });
+    });
+};
+
+// Retrieve all Actvities from the database.
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
+  
+    Activity.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving activities."
+        });
+      });
+};
+
+// Find an single Activity with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Activity.findByPk(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Activity with id=" + id
+        });
+      });
+};
+
+// Update an Activity by the id in the request
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Activity.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Activity was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Activity with id=${id}. Maybe Activity was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Activity with id=" + id
+        });
+      });
+};
+
+// Delete an Activity with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Activity.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Activity was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete Activity with id=${id}. Maybe Activity was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete Activity with id=" + id
+        });
+      });
+};
+
+// Delete all Activities from the database.
+exports.deleteAll = (req, res) => {
+  Activity.destroy({
+        where: {},
+        truncate: false
+      })
+        .then(nums => {
+          res.send({ message: `${nums} Activity were deleted successfully!` });
+        })
+        .catch(err => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while removing all activities."
+          });
+        })
+};
+
+// Find all published Activities
+exports.findAllPublished = (req, res) => {
+  Activity.findAll({ where: { published: true } })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving activities."
+      });
+    });
+};
