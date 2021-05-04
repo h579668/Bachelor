17e4e7ad6d64e7c3b9a48c5efec68cec456a3322
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-04 15:22:52 +0200

    schema er med

--- trymes-b/app/config/db.config.js
+++ trymes-b/app/config/db.config.js
@@ -10,10 +10,12 @@ module.exports = {
     DB: "TryMes",
     dialect: "postgres",
     port: 5434,
+    schema: "user_tables",
     pool: {
       max: 5,  //maximum number of connection in pool
       min: 0,  //minimum number of connection in pool
       acquire: 30000, //maximum time, in milliseconds, that a connection can be idle before being released
       idle: 10000 //maximum time, in milliseconds, that pool will try to get connection before throwing error
     }
+
   };
