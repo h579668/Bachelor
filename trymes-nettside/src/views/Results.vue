f29a793c38757758dfea08045a212dda3fddb741
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-04 10:46:23 +0200

    La inn liste/tabell  over resultatene

--- trymes-nettside/src/views/Results.vue
+++ trymes-nettside/src/views/Results.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
+    <h1> {{ table_of_results }} </h1>
     <canvas id="activity-chart"></canvas>
+    <ResultsList :list_of_results="list_of_results" />
   </div>
 </template>
 
 <script>
 import Chart from "chart.js";
 import activityChartData from "../activity-data.js";
+import ResultsList from "@/components/ResultsList.vue"
 
 export default {
   name: "Restults",
+  components: {
+    ResultsList
+    },
   data() {
     return {
       activityChartData: activityChartData,
+      list_of_results: "Liste over resultatene",
+      table_of_results: "Tabell over resultatene",
     };
   },
   mounted() {
