c6bb27820edb02457843e99375f21cc037ca0ce5
h579668 <42640804+h579668@users.noreply.github.com>
2021-05-05 14:29:01 +0200

    component

---
+++ trymes-nettside/src/components/ChangePassPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="pass">
+    <h1>{{ changePassMsg }}</h1>
+    <form class="newpass-container">
+      <tbody>
+        <table>
+       <!---   <th>{{ loginmsg }}</th>-->
+          <tr>
+            <td>
+              <label>{{ oldPass }}</label>
+            </td>
+            <td>
+              <input class="newpass-input" type="password" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>{{ password }}</label>
+            </td>
+            <input class="newpass-input" type="password" />
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <label>{{ repeatPass }}</label>
+            </td>
+            <input class="newpass-input" type="password" />
+            <td></td>
+          </tr>
+        </table>
+      </tbody>
+      <button @click="change" id="changebtn" class="changebtn newpass-purple">
+        Endre passord
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChangePass",
+  props: {
+    changePassMsg: String,
+  },
+  methods: {
+    change() {
+      window.alert("Passordet ble endret.");
+      this.$router.push({ path: "/admenu" });
+    },
+  },
+  data () {
+    return {
+      oldPass: "Gammelt passord:",
+      password: "Nytt passord:",
+      repeatPass: "Gjenta det nye passordet:",
+    };
+  }
+  
+};
+</script>
+
+<style scoped>
+.pass {
+  border: 2px;
+  border-color: black;
+  border-style: solid;
+  justify-content: center;
+  position: absolute;
+  padding: 0 10% 2% 10%;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+td {
+  text-align: right;
+}
+
+</style>
