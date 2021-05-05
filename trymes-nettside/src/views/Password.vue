05a2f1793005500a665dea9b2db87d370776433d
h579668 <42640804+h579668@users.noreply.github.com>
2021-05-05 14:14:03 +0200

    Endre passord view

---
+++ trymes-nettside/src/views/Password.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pass">
+    <ChangePass changePassMsg="Endre passord" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ChangePass from "@/components/ChangePassPage.vue";
+
+export default {
+  name: "Password",
+  components: {
+    ChangePass,
+  },
+};
+</script>
