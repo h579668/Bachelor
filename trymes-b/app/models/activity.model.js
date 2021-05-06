1a065343d5eed98e0e3d6a5a999da608128f6e3f
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-06 14:07:49 +0200

    Activity

--- trymes-b/app/models/activity.model.js
+++ trymes-b/app/models/activity.model.js
@@ -1,16 +1,11 @@
 'use strict';
+'use strict';
 /*
  * We dropped writing the "s" on the end of the table names
  * The "s" is added automatically when the table is being initialized
 */
 module.exports = (sequelize, Sequelize) => {
     const Activity = sequelize.define("activitie", {
-      associations_id: {
-        type: Sequelize.INTEGER
-      },
-      age_id: {
-        type: Sequelize.INTEGER
-      },
       act_name: {
         type: Sequelize.STRING
       },
@@ -25,5 +20,12 @@ module.exports = (sequelize, Sequelize) => {
       },
     });
   
+    Activity.associate = function(models){
+      Activity.belongsToMany(models.user, { through: models.user_activity });
+      Activity.belongsToMany(models.feature,{ through: models.activity_feature });
+      Activity.belongsToMany(models.age);
+      Activity.hasOne(models.assosiation);
+    }
+    
     return Activity;
   };
