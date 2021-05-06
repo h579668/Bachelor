860c26b32de179edcd6f79271ac5714e9251245f
h579690 <42602824+h579690@users.noreply.github.com>
2021-05-06 15:36:05 +0200

    Add files via upload

---
+++ trymes-b/app/models/association.model.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+ * We dropped writing the "s" on the end of the table names
+ * The "s" is added automatically when the table is being initialized
+*/
+module.exports = (sequelize, Sequelize) => {
+    const Association = sequelize.define("association", {
+      ass_name: {
+        type: Sequelize.STRING
+      },
+    });
+  
+    Association.associate = function(models){
+      Association.belongsTo(models.user);
+      Association.belongsToMany(models.activity);
+    }
+    return Association;
+  };
