4e19508e245e883d64eaacf939742414068d621c
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-06 14:13:16 +0200

    Update relations.js

--- trymes-b/app/models/relations.js
+++ trymes-b/app/models/relations.js
@@ -1,3 +1,4 @@
+module.exports = (sequelize, Sequelize) => {
 const User = require("./user.model.js")(sequelize, Sequelize);
 const Age = require("./age_interval.model.js")(sequelize, Sequelize);
 const Activity = require("./activity.model.js")(sequelize, Sequelize);
@@ -13,31 +14,36 @@ const User_Feature = require("./user_feature.model.js")(sequelize, Sequelize);
 const Activity_Feature = require("./activity_feature.model.js")(sequelize, Sequelize);
 
 //many-to-many relation (N-M) in a separate table
-User.belongsToMany(Activity, { through: User_Activity });
-Activity.belongsToMany(User, { through: User_Activity });
+//User.belongsToMany(Activity, { through: User_Activity });
+//Activity.belongsToMany(User, { through: User_Activity });
 
-User.belongsToMany(Feature, { through: User_Feature });
-Feature.belongsToMany(User, { through: User_Feature });
+//User.belongsToMany(Feature, { through: User_Feature });
+//Feature.belongsToMany(User, { through: User_Feature });
 
-Activity.belongsToMany(Feature,{ through: Activity_Feature });
-Feature.belongsToMany(Activity,{ through: Activity_Feature });
+//Activity.belongsToMany(Feature,{ through: Activity_Feature });
+//Feature.belongsToMany(Activity,{ through: Activity_Feature });
 
 //many-to-many relation (N-M)
-Assosiation.belongsToMany(Area);
-Area.belongsToMany(Assosiation);
+//Assosiation.belongsToMany(Area);
+//Area.belongsToMany(Assosiation);
 
-User.belongsToMany(Area);
-Area.belongsToMany(User);
+//User.belongsToMany(Area);
+//Area.belongsToMany(User);
 
-Activity.belongsToMany(Age);
-Age.belongsToMany(Activity);
+//Activity.belongsToMany(Age);
+//Age.belongsToMany(Activity);
+
+//one-to-one relation (1-1)
+//User.hasOne(Age);
+//User.hasOne(Area);
 
 //One-to-many relation (1-N)
-Feature.hasOne(Category);
-Category.belongsToMany(Feature);
+//Feature.hasOne(Category);
+//Category.belongsToMany(Feature);
 
-Activity.hasOne(Assosiation);
-Assosiation.belongsToMany(Activity);
+//Activity.hasOne(Assosiation);
+//Assosiation.belongsToMany(Activity);
 
-User.hasOne(Age);
-Age.belongsToMany(User);
+//User.hasOne(Age);
+//Age.belongsToMany(User);
+};
