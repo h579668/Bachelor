06790c07064250e968769bb1fd3c0bca7604c509
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-06 14:26:07 +0200

    Update server.js

--- trymes-b/server.js
+++ trymes-b/server.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 
+const path = __dirname + '/app/views/';
+
 const app = express();
+
+app.use(express.static(path));
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 
 db.sequelize.sync();
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to TryMes application." });
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
 });
 
+require("./app/routes/activity.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
