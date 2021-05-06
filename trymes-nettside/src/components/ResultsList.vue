447d2ff0a9912077a934d36f5282068f0c3828e6
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-06 14:14:12 +0200

    Update ResultsList.vue

--- trymes-nettside/src/components/ResultsList.vue
+++ trymes-nettside/src/components/ResultsList.vue
@@ -1,35 +1,35 @@
 <template>
-    <div>
-        <table class="tableActivity">
-            <thead>
-                <tr>
-                    <th>Aktivitet</th>
-                    <th>Telefon</th>
-                    <th>E-post</th>
-                    <th>Forbund</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in activities" :key="item.activity">
-                    <td> {{ item.activity }}</td>
-                    <td> {{ item.tlf }}</td>
-                    <td> {{ item.epost }}</td>
-                    <td> {{ item.associations }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+  <div>
+    <table class="tableActivity">
+      <thead>
+        <tr>
+          <th>Aktivitet</th>
+          <th>Telefon</th>
+          <th>E-post</th>
+          <th>Forbund</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in labels" :key="index">
+          <td>{{ item.activity }}</td>
+          <td>{{ item.tel }}</td>
+          <td>{{ item.email }}</td>
+          <td>{{ item.associations }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-//import labels from "../activity-data.js";
+import labels from "@/services/ActivityDataService.js";
     export default {
         name: "ResultsList",
         props: {
             list_of_results:String,
         },
         data(){
-            return { 
+            return { /*
                 activities: [
                     { activity: "Skateboard", tlf:"454 64 166", epost:"henning@norb.no", associations: "Brettforbundet"}, 
                     { activity: "Snøbrett", tlf:"922 49 073", epost:"magnus@snowboardforbundet.no", associations: "Brettforbundet"}, 
@@ -41,25 +41,46 @@
                     { activity: "Amerikansk fotball", tlf:"472 01 596", epost:"tone.sparby@nif.idrett.no", associations: "Norges amerikanske idretters forbund"},  
                     { activity: "Cheerleading", tlf:"986 66 494", epost:"daniela.ohnstad@nif.idrett.no", associations: "Norges amerikanske idretters forbund"},  
                     { activity: "Freesbeegolf", tlf:"454 64 166", epost:"tone.sparby@nif.idrett.no", associations: "Norges amerikanske idretters forbund"}, 
-                ],
+                ],*/
+             
+                labels: [],
+                activity:"",
+                tel: "",
+                email:"",
+                assosiations:"",
+
              };
-        }
-    };
+        },
+        methods: {
+            retrieveLabels() {
+                labels.getAll()
+                    .then(response => {
+                    this.labels = response.data;
+                    console.log(response.data);
+                })
+                .catch(e => {
+                console.log(e);
+            });
+        },
+    },
+    mounted() {
+        this.retrieveTutorials();
+    } 
+};
 </script>
 
 <style scoped>
-
 .tableActivity {
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
 }
-   
+
 tbody {
-    text-align: left;
+  text-align: left;
 }
 
 li {
-    list-style-type: none;
+  list-style-type: none;
 }
 </style>
