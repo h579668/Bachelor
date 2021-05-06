d0f24db5a012862fb3ae1b1d5f613a60a0603ad4
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-06 14:10:30 +0200

    Update feature.model.js

--- trymes-b/app/models/feature.model.js
+++ trymes-b/app/models/feature.model.js
@@ -4,14 +4,19 @@
  * The "s" is added automatically when the table is being initialized
 */
 module.exports = (sequelize, Sequelize) => {
-    const Feature= sequelize.define("activitie", {
+    const Feature= sequelize.define("feature", {
       categories_id: {
         type: Sequelize.INTEGER
       },
       feat_name: { //Feature name
-        type: Sequelize.String
+        type: Sequelize.STRING
       },
     });
+    Feature.associate = function(models){
+      Feature.belongsToMany(User, { through: models.user_feature });
+      Feature.belongsToMany(Activity,{ through: models.activity_feature });
+      Feature.hasOne(models.category);
+    }
   
     return Feature;
   };
