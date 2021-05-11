09cf21551cbd768ef530c573a50b090023ad9005
h579690 <h579690@stud.hvl.no>
2021-05-11 18:16:58 +0200

    activityList

--- trymes-nettside/src/components/ActivityList.vue
+++ trymes-nettside/src/components/ActivityList.vue
@@ -1,68 +1,104 @@
 <template>
      <div>
+        <h1> {{msg}} </h1>
         <table class="tableActivity">
             <thead>
                 <tr>
-                    <th>Aktivitet</th>
-                    <th>Telefon</th>
-                    <th>E-post</th>
-                    <th>Forbund</th>
+                    <th rowspan="2">Aktivitet</th>
+                    <th colspan="2" v-for="(item, index) in categories" :key="index">{{ item.categories_name }}</th>
+                </tr>
+                <tr>
+                    <th v-for="(item, index) in features" :key="index">{{ item.features_name }}</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="item in activities" :key="item.activity">
-                    <td> {{ item.activity }}</td>
-                    <td> {{ item.tlf }}</td>
-                    <td> {{ item.epost }}</td>
-                    <td> {{ item.associations }}</td>
+                <tr v-for="(item, index) in activities" :key="index">
+                    <td>{{ item.activities_name }}</td>
+                    <td>1</td>
+                    <td>3</td>
+                    <td>2</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>3</td>
                 </tr>
             </tbody>
         </table>
-        <!-- <div class="col-md-6">
-        <h4>{{msg}}</h4>
-        <ul class="list-group">
-            <li class="list-group-item"
-            :class="{ active: index == currentIndex }"
-            v-for="(activity, index) in activities"
-            :key="index"
-            @click="setActiveActivity(activity, index)"
-            >
-            {{ activity.navn }}
-            </li>
-        </ul>
-
-        </div> -->
+        
     </div>
 </template>
 
 <script>
+import ActivityDataService from "@/services/ActivityDataService.js";
+import CategoryDataService from "@/services/CategoryDataService.js";
+import FeatureDataService from "@/services/FeatureDataService.js";
 
-export default {
-  name: "ActivityList",
+  export default {
+    name: "ActivityList",
+    props: {
+      msg: String,
+    },
+    data(){
+      return { 
+        activities: [],
+        activities_name: "",
 
-  data() {
-    return {
-      activities: [
-            { activity: "Skateboard", tlf:"454 64 166", epost:"henning@norb.no", associations: "Brettforbundet"}, 
-            { activity: "Snøbrett", tlf:"922 49 073", epost:"magnus@snowboardforbundet.no", associations: "Brettforbundet"}, 
-            { activity: "Baneskyting", tlf:"975 92 754", epost:"ofh@dfs.no", associations: "Det frivillige Skyttervesen (ingen tilknytning til NIF)"}, 
-            { activity: "Fallskjermhopping", tlf:"907 04 646", epost:"janerik.wang@nlf.no", associations: "Luftsportforbundet"}, 
-            { activity: "Bueskyting", tlf:"", epost:"", associations: "NIF/Norges Bueskytterforbund. Og Norsk Langbuelag (NL)"}, 
-            { activity: "Enduro motorsykkel", tlf:"415 64 430", epost:"a.sletholt@gmail.com", associations: "NMF"}, 
-            { activity: "Aikido", tlf:"918 64 797", epost:"idalagosandersen@gmail.com", associations: "Norges Aikidoforbund"}, 
-            { activity: "Amerikansk fotball", tlf:"472 01 596", epost:"tone.sparby@nif.idrett.no", associations: "Norges amerikanske idretters forbund"},  
-            { activity: "Cheerleading", tlf:"986 66 494", epost:"daniela.ohnstad@nif.idrett.no", associations: "Norges amerikanske idretters forbund"},  
-            { activity: "Freesbeegolf", tlf:"454 64 166", epost:"tone.sparby@nif.idrett.no", associations: "Norges amerikanske idretters forbund"}, 
-        ],
-      currentActivity: null,
-      currentIndex: -1,
-      navn: ""
-    };
-  },
+        categories: [],
+        categories_name: "",
+
+        features: [],
+        features_name: "",
+      };
+    },
+    methods: {
+      retrieveActivities() {
+        ActivityDataService.getAll()
+            .then(response => {
+            this.activities = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+          console.log(e);
+        });
+      },
+      retrieveCategories() {
+        CategoryDataService.getAll()
+            .then(response => {
+            this.categories = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+          console.log(e);
+        });
+      },
+      retrieveFeatures() {
+        FeatureDataService.getAll()
+            .then(response => {
+            this.features = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+          console.log(e);
+        });
+      },
+      refreshList() {
+        this.retrieveActivities();
+        this.retrieveCategories();
+        this.retrieveFeatures();
+      
+      },
+    },
+    mounted() {
+      this.retrieveActivities();
+      this.retrieveCategories();
+      this.retrieveFeatures();
+    } 
+  
 };
 </script>
 
-<style>
+<style scoped>
 .tableList {
     padding-top: 30px;
     text-align: left;
@@ -71,4 +107,6 @@ export default {
 .tableList, th, td {
     padding-right: 50px;
 }
+
+
 </style>
