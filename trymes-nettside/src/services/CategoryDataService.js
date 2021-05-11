ba1d6829b6916d31b4b039c4b98028d7cacd6a38
h579690 <42602824+h579690@users.noreply.github.com>
2021-05-11 12:10:04 +0200

    Add files via upload

---
+++ trymes-nettside/src/services/CategoryDataService.js
@@ -0,0 +1,35 @@
+import http from "../http-commons.js";
+
+class CategoryDataService {
+  getAll() {
+    return http.get("/categories");
+  }
+
+  findOne(id) {
+    return http.get(`/categories/${id}`);
+  }
+
+  create(data) {
+    return http.post("/categories", data);
+  }
+
+  update(id, data) {
+    return http.put(`/categories/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/categories/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/categories`);
+  }
+
+  findByTitle(navn) {
+    return http.get(`/categories?navn=${navn}`);
+  }
+}
+
+
+
+export default new CategoryDataService(); 
