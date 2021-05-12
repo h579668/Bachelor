90932168c40428527712c009b8d24f72fa1cbf4f
h579690 <h579690@stud.hvl.no>
2021-05-12 14:51:12 +0200

    ny aktivitetliste

--- trymes-nettside/src/components/ActivityList.vue
+++ trymes-nettside/src/components/ActivityList.vue
@@ -1,7 +1,109 @@
 <template>
      <div>
         <h1> {{msg}} </h1>
-        <table class="tableActivity">
+   
+        <!-- Tab links -->
+        <div class="tab">
+          <th v-for="(item, index) in categories" :key="index">
+            <button class="tablinks" @click="openCity(event, item.categories_name)">{{ item.categories_name }}</button>
+          </th>
+        </div>
+      
+      
+        <!-- Tab content -->
+        <div id="Fysiske egenskaper" class="tabcontent">
+          <h3>Fysiske egenskaper</h3>
+            <table>
+              <thead>
+                <!--<tr>
+                    <th v-for="(item, index) in features" :key="index">{{ item.features_name }}</th>
+                </tr>-->
+                <tr>
+                  <th>Aktivitet</th>
+                  <th>Styrke</th>
+                  <th>Spenst</th>
+                  <th>Utholdenhet</th>
+                  <th>Bevegelighet</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in activities" :key="index">
+                  <td>{{ item.activities_name }}</td>
+                  <td>1</td>
+                  <td>3</td>
+                  <td>2</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+
+        <div id="Koordinative egenskaper" class="tabcontent">
+          <h3>Koordinative egenskaper</h3>
+          <table>
+              <thead>
+                <!--<tr>
+                    <th v-for="(item, index) in features" :key="index">{{ item.features_name }}</th>
+                </tr>-->
+                <tr>
+                  <th>Aktivitet</th>
+                  <th>Balanse</th>
+                  <th>Rytme</th>
+                  <th>Øye-fot</th>
+                  <th>Øye-hånd</th>
+                  <th>Romorientering</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in activities" :key="index">
+                  <td>{{ item.activities_name }}</td>
+                  <td>1</td>
+                  <td>3</td>
+                  <td>2</td>
+                  <td>2</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+            </table> 
+        </div>
+
+        <div id="Psykologiske faktorer" class="tabcontent">
+          <h3>Psykologiske faktorer</h3>
+          
+        </div>
+
+        <div id="Konkurranse" class="tabcontent">
+          <h3>Konkurranse</h3>
+          <table>
+              <thead>
+                <!--<tr>
+                    <th v-for="(item, index) in features" :key="index">{{ item.features_name }}</th>
+                </tr>-->
+                <tr>
+                  <th>Aktivitet</th>
+                  <th>Estetikk/Kunst (Subjektivt)</th>
+                  <th>Kamp (vinner-taper)</th>
+                  <th>MSK (Meter, Sekund, Kilo) (Rangering)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in activities" :key="index">
+                  <td>{{ item.activities_name }}</td>
+                  <td></td>
+                  <td>Ja</td>
+                  <td></td>
+                  
+                </tr>
+              </tbody>
+            </table> 
+        </div>
+
+        <div id="Rammer" class="tabcontent">
+          <h3>Rammer</h3>
+          
+        </div>
+
+        <!--<table class="tableActivity">
             <thead>
                 <tr>
                     <th rowspan="2">Aktivitet</th>
@@ -24,7 +126,7 @@
                     <td>3</td>
                 </tr>
             </tbody>
-        </table>
+        </table>-->
         
     </div>
 </template>
@@ -52,6 +154,27 @@ import FeatureDataService from "@/services/FeatureDataService.js";
       };
     },
     methods: {
+      openCity(evt, tabName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+      },
+
       retrieveActivities() {
         ActivityDataService.getAll()
             .then(response => {
@@ -62,6 +185,7 @@ import FeatureDataService from "@/services/FeatureDataService.js";
           console.log(e);
         });
       },
+
       retrieveCategories() {
         CategoryDataService.getAll()
             .then(response => {
@@ -106,6 +230,42 @@ import FeatureDataService from "@/services/FeatureDataService.js";
 }
 .tableList, th, td {
     padding-right: 50px;
+}
+
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
+
+/* Style the buttons that are used to open the tab content */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
 }
 
 
--- trymes-nettside/src/views/Activities.vue
+++ trymes-nettside/src/views/Activities.vue
@@ -16,7 +16,7 @@ export default {
   },
   data() {
     return {
-      msg: "Liste over aktiviteter:",
+      msg: "Aktiviteter med egenskaper",
     }
   },
 };
