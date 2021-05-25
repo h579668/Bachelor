4053c891061ba3a97e3f6f258b87ba2f2953e8c9
h576347 <42640829+h576347@users.noreply.github.com>
2021-05-25 12:58:28 +0200

    Update main.js

--- trymes-nettside/src/main.js
+++ trymes-nettside/src/main.js
@@ -4,9 +4,7 @@ import router from "./router";
 import store from "./store";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import ZkTable from 'vue-table-with-tree-grid'
 
-Vue.use(ZkTable);
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
