eac35214a7b739516e2ed278fd388aaf1ae54060
h579690 <h579690@stud.hvl.no>
2021-06-01 17:39:04 +0200

    funker igjen

--- trymes-nettside/src/views/Questions.vue
+++ trymes-nettside/src/views/Questions.vue
@@ -5,42 +5,41 @@
     </div>
     <!-- role="main" means that it is the main content on the page-->
     <div role="main"> 
-    <fieldset>
-    <legend> {{ information }}</legend>
-    <!--v-for-loop to get all the quiestions from the quiz table
-      the table is not yet created in the database, but it should be in the future-->
-    <div v-for="cat in questionCategory" :key="cat.questions_category_id">
-      <h1>{{ cat.title }}</h1>
-      <div id="questions" v-for="item in cat.questions" :key="item.questions_id">
-        <h2>{{ item.feature}}</h2>
-        <p>{{ item.description }}</p>
-        <div id="inputRadio" name="inputFields">
-          
-          <input type="radio" :id="item.id + 'a'" :name="item.id" :value="3" />
-          <label :for="item.id + 'a'"> Synes det er kjempegøy </label> <br />
+      <fieldset>
+        <legend> {{ information }}</legend>
+        <!--v-for-loop to get all the quiestions from the quiz table
+        the table is not yet created in the database, but it should be in the future-->
+        <div v-for="cat in questionCategory" :key="cat.questions_category_id">
+          <h1>{{ cat.title }}</h1>
+          <div id="questions" v-for="item in cat.questions" :key="item.questions_id">
+            <h2>{{ item.feature}}</h2>
+            <p>{{ item.description }}</p>
+            <div id="inputRadio">
+            
+              <input type="radio" :id="item.questions_id + 'a'" :name="item.questions_id" :value="3" />
+              <label :for="item.questions_id + 'a'"> Synes det er kjempegøy </label> <br />
 
-          <input type="radio" :id="item.id + 'b'" :name="item.id" :value="2"  />
-          <label :for="item.id + 'b'"> Helt greit </label><br />
+              <input type="radio" :id="item.questions_id + 'b'" :name="item.questions_id" :value="2"  />
+              <label :for="item.questions_id + 'b'"> Helt greit </label><br />
 
-          <input type="radio" :id="item.id + 'c'" :name="item.id" :value="1"  />
-          <label :for="item.id + 'c'"> Liker ikke i det hele tatt </label>
-          
+              <input type="radio" :id="item.questions_id + 'c'" :name="item.questions_id" :value="1"  />
+              <label :for="item.questions_id + 'c'"> Liker ikke i det hele tatt </label>
+
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-
-    </fieldset>
+      </fieldset>
     </div>
 
     <div>
       <!-- disabled button because it is only one page at the moment-->
-      <button @click="RegisterUser" id="btn-navigation" class="btn-navigation navigation-purple">
+      <button @click="RegisterUser" class="btn-navigation" >
         Gå tilbake
       </button>
       <!--<button @click="nextQuestions" id="btn-navigation" class="btn-navigation navigation-purple">
         Neste
       </button>-->
-     <button @click="result" id="btn-navigation" class="btn-navigation navigation-purple">
+     <button @click="result" class="btn-navigation" >
         Gå til resultater
       </button>
 
@@ -55,6 +54,7 @@
 <script>
 import QuestionDataService from "@/services/QuestionDataService.js"
 import QuestionCategoryDataService from "@/services/QuestionCategoryDataService.js"
+import UserDataService from "@/services/UserDataService.js"
 
 export default {
   name: "QuestionPage",
@@ -68,40 +68,11 @@ export default {
 
       questionCategory: [],
       title: "",
-
-      /*quizItems: [
-        {
-          id: "1",
-          question: "Styrkeøvelser",
-          info: "(armhevinger, knebøy, situps, vekter osv)",
-        },
-        {
-          id: "2",
-          question: "Spenst/hurtighet",
-          info: "(hopping, sprint, kasting osv)",
-        },
-        {
-          id: "3",
-          question: "Utholdenhet",
-          info: "(gåturer, jogging, svømme langt, sykkeltur osv)",
-        },
-        {
-          id: "4",
-          question: "Bevegelighet",
-          info: "(tøying, spagat, utstrekking osv)",
-        },
-        {
-          id: "5",
-          question: "Balanse",
-          info:
-            "(gå på bom/line, stå på en fot, bygge store tårn, stå på tærne osv)",
-        },
-      ],*/
      
     };
   },
   methods: {
-    radioButtons(){
+    /*radioButtons(){
       let inputs = inputFields.elements;
       let radios =[];
       let result={ features_id: "", users_features_values: ""};
@@ -121,7 +92,7 @@ export default {
             result[i].features_id = radios[i].id;
         }
     }
-    },
+    },*/
     saveUserWithInput(){
       let newUser = {
         age_id: this.select_age,
@@ -181,42 +152,46 @@ export default {
 <style scoped>
 .quiz {
   margin-left: auto;
-   margin-right: auto;
-   margin-top: 20px;
+  margin-right: auto;
+  margin-top: 20px;
 }
 
 #questions {
   padding-bottom: 20px;
   padding-top: 10px;
   margin-left: auto;
-   margin-right: auto;
- }
-
- input[type="radio"]:hover, label:hover{
-   background-color: #548687;
-   color: #ffff;
- }
-
- #inputRadio {
-   margin: 0 auto;
-   width: auto;
-   text-align: left;
-   display: table;
-   line-height: 35px;
-   font-size: 20px;
+  margin-right: auto;
 }
 
-#btn-navigation {
+input[type="radio"]:hover, label:hover{
+  background-color: #548687;
+  color: #ffff;
+  cursor: pointer;
+}
+
+
+#inputRadio {
+  margin: 0 auto;
+  width: auto;
+  text-align: left;
+  display: table;
+  line-height: 35px;
+  font-size: 20px;
+}
+
+.btn-navigation {
   background-color: #424242;
   color: #ffffff;
   font-size: 30px;
 }
 
-#btn-navigation:hover {
-   background-color: #548687;
- }
- fieldset{
-    border-color: #ffff;
-    border-style: solid;
- }
+.btn-navigation:hover {
+  background-color: #548687;
+  cursor: pointer;
+}
+
+fieldset{
+  border-color: #ffff;
+  border-style: solid;
+}
 </style>
